0db46f3c23c03adef125c253d2e0676a72102cb5
10daer <afolabireedwan@gmail.com>
2024-05-25 13:21:26 +0100

    fixes some bugs

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+
+let supabaseUrl, supabaseKey;
+
+if (import.meta.env.MODE === "development") {
+  // Use dev keys
+  supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+} else {
+  // Use .env variables
+  supabaseUrl = process.env.SUPABASE_URL;
+  supabaseKey = process.env.SUPABASE_KEY;
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+export { supabaseUrl };
