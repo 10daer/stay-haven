87b062b328216cf0f7f66f4dedc3b1887e6c1cfb
10daer <114252584+10daer@users.noreply.github.com>
2024-05-25 16:01:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
           <Routes>
             {console.log(Routes)}
             <Route element={<AppLayout />}>
+              {console.log(Route)}
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="settings" element={<Settings />} />
