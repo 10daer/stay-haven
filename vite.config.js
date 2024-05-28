c66db4eaef89594c98029e61cda2657fe6988d92
10daer <afolabireedwan@gmail.com>
2024-05-28 09:44:47 +0100

    Modified

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// // import eslint from "vite-plugin-eslint";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import eslint from "vite-plugin-eslint";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    open: true,
-  },
 });
