fb916c8b7c0ca25a7c69b63d5424847c864a13f3
10daer <afolabireedwan@gmail.com>
2024-05-31 01:49:37 +0100

    modified Sidebar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import Uploader from "../data/Uploader";
+// import Uploader from "../data/Uploader";
 import { devices } from "../styles/MediaQueries";
 
 const StyledSidebar = styled.aside`
